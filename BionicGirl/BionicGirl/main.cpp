64af2246fead08018702f2b8cac0efa110aa0419
Haris Kljajic <devhariis@gmail.com>
2019-10-01 21:06:11 +0200

    WIP time step

--- BionicGirl/BionicGirl/main.cpp
+++ BionicGirl/BionicGirl/main.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 #include "MenuState.h"
 
-const int FPS = 60; 
-const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char* argv[]){
-	Uint64 currentFrame = SDL_GetPerformanceCounter();
-	Uint64 lastFrame = 0;
-	Uint32 startclock, currentFPS = 0;
+	Uint32 currentFrame = SDL_GetTicks();
+	Uint32 lastFrame = 0;
 	float deltaTime = 0;
+	Uint32 frameStart, frameTime;
 
 	GameInst::Instance()->init("Bionic Girl", "./assets/icon.png", 1024, 720, false);
 	GameInst::Instance()->change_state(MenuState::Instance());
 
 	while (GameInst::Instance()->running()){
-		startclock = SDL_GetTicks();
-
-		lastFrame = currentFrame;
-		currentFrame = SDL_GetPerformanceCounter();
-
-		deltaTime = ((currentFrame - lastFrame) * 1000.0f / (float)SDL_GetPerformanceFrequency());
+		frameStart = SDL_GetTicks();
+		frametime
 
 		GameInst::Instance()->handle_events();
-		GameInst::Instance()->update(deltaTime);
+
+		//lastFrame = currentFrame;
+		//currentFrame = SDL_GetTicks();
+
+		//deltaTime = ((currentFrame - lastFrame) * 1000.0f / SDL_GetTicks());
+		GameInst::Instance()->update(0);
 		GameInst::Instance()->draw();
 
-		startclock = SDL_GetTicks();
+		frameTime = SDL_GetTicks() - frameStart;
 
-		if (deltaTime != 0)
-			currentFPS = 1000 / deltaTime;
-
-		cout << currentFPS;
+		cout << SDL_GetTicks();
 		cout << "\n";
 	}
 
